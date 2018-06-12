9e035bfa26e0b92f680bfcf4ee8d91e3d41f02bd
Jonathan Kwinta <jkwinta@gmail.com>
2018-06-12 10:30:15 -0400

    Read/write phoneme dict to file

--- get_phoneme_dictionary.py
+++ get_phoneme_dictionary.py
@@ -1,7 +1,17 @@
 from urllib import request
+import os
+from sys import stderr
+
+DICTIONARY_DIR = './files'
+
+if not os.path.isdir(DICTIONARY_DIR):
+    if os.path.isfile(DICTIONARY_DIR):
+        print('You have a file where a directory should be in {}'.format(DICTIONARY_DIR), file=stderr)
+        exit(1)
+    os.mkdir(DICTIONARY_DIR)
+    
 
 DICTIONARY_URL = 'http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b'
-
 
 word_to_phoneme = {}
 
@@ -11,10 +21,5 @@ with request.urlopen(DICTIONARY_URL) as f:
         if line and not line.startswith(';;;'):
             line = line.split()
             word_to_phoneme[line[0]] = [''.join([c for c in ph if not c.isdigit()]) for ph in line[1:]]
-
-
-
-
-
 
 pass
