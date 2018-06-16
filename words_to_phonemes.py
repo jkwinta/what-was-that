9f9b62f29a859f1d713b59276a72b4b1f60e6668
Jonathan Kwinta <jkwinta@localhost.localdomain>
2018-06-15 21:53:22 -0400

    Get list of phonemes

--- words_to_phonemes.py
+++ words_to_phonemes.py
@@ -1,2 +1,9 @@
+from get_phoneme_dictionary import get_phoneme_dictionary
+
+
 def words_to_phonemes(words):
-    pass
+    word_to_phoneme_dict = get_phoneme_dictionary()
+    result = []
+    for word in words.split():
+        result.extend(word_to_phoneme_dict.get(word.upper(), [-1]))
+    return result
