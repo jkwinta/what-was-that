0dce78f92977fdd4c60c8cb033c1095e12dc6bc8
Jonathan Kwinta <jkwinta@localhost.localdomain>
2018-06-15 20:33:54 -0400

    Refactored in function returning dict, should be static somewhere

--- get_phoneme_dictionary.py
+++ get_phoneme_dictionary.py
@@ -1,25 +1,28 @@
 from urllib import request
 import os
-from sys import stderr
 
-DICTIONARY_DIR = './files'
 
-if not os.path.isdir(DICTIONARY_DIR):
-    if os.path.isfile(DICTIONARY_DIR):
-        print('You have a file where a directory should be in {}'.format(DICTIONARY_DIR), file=stderr)
-        exit(1)
-    os.mkdir(DICTIONARY_DIR)
-    
-
+DICTIONARY_FILE = 'cmudict-0.7b'
 DICTIONARY_URL = 'http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b'
 
-word_to_phoneme = {}
 
-with request.urlopen(DICTIONARY_URL) as f:
-    for bytes_line in f:
-        line = bytes_line.decode('latin-1').strip()
+def get_phoneme_dictionary():
+    if not os.path.isfile('./' + DICTIONARY_FILE):
+        print("Downloading dictionary...")
+        req_response = request.urlopen(DICTIONARY_URL)
+        phoneme_dict_str = req_response.read().decode('latin-1')
+        write_dict = ''
+        while write_dict.lower() not in ('y', 'n'):
+            write_dict = input("Write phoneme dictionary to file for future you? [y/n]").strip()
+        if write_dict == 'y':
+            with open('./' + DICTIONARY_FILE, 'w') as f:
+                f.write(phoneme_dict_str)
+    else:
+        with open('./' + DICTIONARY_FILE) as f:
+            phoneme_dict_str = f.read()
+    word_to_phoneme = {}
+    for line in phoneme_dict_str.split('\n'):
         if line and not line.startswith(';;;'):
             line = line.split()
             word_to_phoneme[line[0]] = [''.join([c for c in ph if not c.isdigit()]) for ph in line[1:]]
-
-pass
+    return word_to_phoneme
