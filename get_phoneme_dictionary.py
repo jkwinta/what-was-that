d38d66cafeaf0e766bf6e8b12f409f75e8342d07
Jonathan Kwinta <jkwinta@gmail.com>
2018-06-16 01:24:12 -0400

    Use tuples, formatting

--- get_phoneme_dictionary.py
+++ get_phoneme_dictionary.py
@@ -1,7 +1,6 @@
 from urllib import request
 import os
 
-
 DICTIONARY_FILE = 'cmudict-0.7b'
 DICTIONARY_URL = 'http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b'
 
@@ -13,7 +12,7 @@ def get_phoneme_dictionary():
         phoneme_dict_str = req_response.read().decode('latin-1')
         write_dict = ''
         while write_dict.lower() not in ('y', 'n'):
-            write_dict = input("Write phoneme dictionary to file for future you? [y/n]").strip()
+            write_dict = input("Write phoneme dictionary to file for future you? [y/n] ").strip()
         if write_dict == 'y':
             with open('./' + DICTIONARY_FILE, 'w') as f:
                 f.write(phoneme_dict_str)
@@ -24,5 +23,5 @@ def get_phoneme_dictionary():
     for line in phoneme_dict_str.split('\n'):
         if line and not line.startswith(';;;'):
             line = line.split()
-            word_to_phoneme[line[0]] = [''.join([c for c in ph if not c.isdigit()]) for ph in line[1:]]
+            word_to_phoneme[line[0]] = tuple(''.join([c for c in ph if not c.isdigit()]) for ph in line[1:])
     return word_to_phoneme
