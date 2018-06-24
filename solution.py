24c761a3b6ec05e08b1d0663212e984682529e31
Jonathan Kwinta <jkwinta@localhost.localdomain>
2018-06-23 20:39:32 -0400

    Wrapper class

---
+++ solution.py
@@ -0,0 +1,12 @@
+class Instance:
+    def __init__(self, instance):
+        self.input = instance
+        # self.words = instance.split()
+        self.words = None
+        self.phonemes = None
+        self.next = None
+        self.array = None
+        self.solution = None
+
+    def get_solution(self):
+        pass
