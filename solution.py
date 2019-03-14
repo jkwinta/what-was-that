3dd6e8129a6c56a3ab3e3efd1f1e0573ed2807fc
Jonathan Kwinta <jkwinta@gmail.com>
2019-03-13 23:29:56 -0400

    Get piecewise solutions in lists

--- solution.py
+++ solution.py
@@ -6,6 +6,8 @@ class Instance:
     def __init__(self, instance):
         self.input = instance.strip()
         self.words = None
+        self.phonemes = None
+        self.composite_dict = {}
 
     def get_words(self):
         if self.words is None:
@@ -13,22 +15,53 @@ class Instance:
         return self.words
 
     def get_phonemes(self):
-        phonemes = []
-        d = phoneme_dictionaries.get_word_to_phonemes_dict()
-        for word in self.get_words():
-            word_upper = word.upper()
-            if word_upper in d:
-                if not phonemes or not isinstance(phonemes[-1], list):
-                    phonemes.append(d[word_upper])
+        if self.phonemes is None:
+            phonemes = []
+            d = phoneme_dictionaries.get_word_to_phonemes_dict()
+            for word in self.get_words():
+                word_upper = word.upper()
+                if word_upper in d:
+                    if not phonemes or not isinstance(phonemes[-1], list):
+                        phonemes.append(d[word_upper])
+                    else:
+                        last_phonemes = phonemes[-1]
+                        phonemes[-1] = []
+                        for first in last_phonemes:
+                            for second in d[word_upper]:
+                                phonemes[-1].append(first + second)
                 else:
-                    last_phonemes = phonemes[-1]
-                    phonemes[-1] = []
-                    for first in last_phonemes:
-                        for second in d[word_upper]:
-                            phonemes[-1].append(first + second)
-            else:
-                phonemes.append(word)
-        return phonemes
+                    phonemes.append(word)
+            self.phonemes = phonemes
+        return self.phonemes
+
+    def get_solution(self, phonemes):
+        if phonemes not in self.composite_dict:
+            solution = []
+            phonemes_to_word = phoneme_dictionaries.get_phonemes_to_word_dict()
+            # phonemes are in the regular dictionary
+            solution.extend(phonemes_to_word.get(phonemes, []))
+            # phonemes are two solutions concatenated:
+            for k in range(1, len(phonemes)):
+                first = self.get_solution(phonemes[:k])
+                second = self.get_solution(phonemes[k:])
+                for f in first:
+                    for s in second:
+                        sol = f + ' ' + s
+                        if sol not in solution:
+                            solution.append(sol)
+            self.composite_dict[phonemes] = solution
+        return self.composite_dict[phonemes]
+
+    def get_all_solutions(self):
+        partial_sol_list = []
+        for part in self.get_phonemes():
+            if isinstance(part, list):
+                partial_sol_list.append([])
+                for phonemes in part:
+                    partial_sol_list[-1].extend(self.get_solution(phonemes))
+            else:  # str
+                partial_sol_list.append([part])
+        return partial_sol_list
 
 
 class SubInstance:
